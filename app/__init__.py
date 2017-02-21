c4323e9e825aa3433ca8c68dcf62b8c0cccf3061
olof <olof.olsson@snd.gu.se>
2017-02-21 10:37:11 +0000

    adding blank lines before functions
    git-svn-id: https://svn.spraakbanken.gu.se/project/skbl.se/trunk@23 f131e4a3-7366-4ca9-96e2-4d333cd68754

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config.from_pyfile('../config.cfg')
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     lang = request.path[1:].split('/', 1)[0]
@@ -20,6 +21,7 @@ def get_locale():
     else:
         return request.accept_languages.best_match(['sv', 'en'])
 
+
 def serve_static_page(page, title=''):
     set_language_swith_link(page)
     
@@ -30,23 +32,27 @@ def serve_static_page(page, title=''):
                             content = data.decode('utf-8'), 
                             title = title)
 
+
 def set_language_swith_link(route, fragment=None):
     if(get_locale() == 'en'):
-        g.switch_language = {'url': url_for(route+'_sv'), 'label' : 'Svenska'}
+        g.switch_language = {'url': url_for(route+'_sv'), 'label': 'Svenska'}
     else:
-        g.switch_language = {'url': url_for(route+'_en'), 'label':'English'}
+        g.switch_language = {'url': url_for(route+'_en'), 'label': 'English'}
     if(fragment != None):
         g.switch_language['url'] += '/'+fragment
+
 
 @app.before_request
 def func():
     g.babel = Babel
     g.language = get_locale()
 
+
 @app.context_processor
 def inject_custom():
     d = {'lurl_for': lambda ep, **kwargs: url_for(ep+'_'+g.language, **kwargs)}
     return d
+
 
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -12,15 +12,18 @@ def start():
     set_language_swith_link("index")
     return render_template('page.html', content='skbl.se')
 
+
 @app.route("/en/about-skbl", endpoint="about-skbl_en")
 @app.route("/sv/om-skbl", endpoint="about-skbl_sv")
 def about_skbl():
     return serve_static_page("about-skbl", gettext("About SKBL"))
 
+
 @app.route("/en/about-us", endpoint="about-us_en")
 @app.route("/sv/om-oss", endpoint="about-us_sv")
 def about_us():
     return serve_static_page("about-us", gettext("About us"))
+
 
 @app.route("/en/contact", endpoint="contact_en")
 @app.route("/sv/kontakt", endpoint="contact_sv")
@@ -36,8 +39,9 @@ def keyword_index():
                             content = 'keyword index', 
                             title = gettext("Keywords"))
 
+
 @app.route("/en/keyword/<keyword>", endpoint="keyword_en")
 @app.route("/sv/nyckelord/<keyword>", endpoint="keyword_sv")
 def keyword(keyword=None):
-    set_language_swith_link("keyword", keyword)
+    set_language_swith_link("keyword_index", keyword)
     return render_template('page.html', content = keyword)
